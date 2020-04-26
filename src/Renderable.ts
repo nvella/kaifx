386f17e4c2f476e275944ba4ef8b554362a06310
Nick Vella <nick@nxk.io>
2020-04-26 23:13:38 +1000

    Renderable: slim up scope of class, move concerns elsewhere

--- src/Renderable.ts
+++ src/Renderable.ts
@@ -1,6 +1,5 @@
 export default abstract class Renderable {
     private _unsureRoot?: HTMLElement;
-    protected _children: Renderable[] = [];
 
     protected get _root(): HTMLElement {
         if(!this._unsureRoot) throw 'no root to render in (did you forget to call init()?)';
@@ -12,31 +11,10 @@ export default abstract class Renderable {
     }
 
     abstract init(parent: HTMLElement): void;
+    abstract render(force: boolean): void;
+    dispose(): void { }    
     
     needsInit(): boolean {
         return !this._unsureRoot;
-    }
-
-    needsRender(): boolean {
-        // simple short circuit needsRender condition check
-        for(var child of this._children)
-        {
-            if(child.needsRender()) return true;
-        }
-        return false;
-    }
-
-    addChild(child: Renderable)
-    {
-        this._children.push(child);
-    }
-
-    render(force: boolean = false): void {
-        if(!force && !this.needsRender()) console.log(`no render required for ${this}`);
-        
-        for(var child of this._children)
-        {
-            if(force || child.needsRender()) child.render();
-        }
     }
 }
